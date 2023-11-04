5041ebd6502ef641e812d4d733208552e6449268
Alex Mattoni <43799350+alexmattoni@users.noreply.github.com>
2023-11-04 12:35:43 -0400

    svelte template

---
+++ front/src/routes/ems/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+
+    export let data: PageData;
+</script>
+
+<h1>EMS Report</h1>
+
+<p> Input Patient Information</p>
+
+<form>
+<label for="ptfirstname"> Patient Name</label> <br>
+<input type="text" id="ptfirstname"> <br>
+<input type="text" id="ptlastname"> <br>
+
+<label for="ptDOB"> DOB </label>
+<input type="date" id = "ptDOB"> <br>
+
+<label for="ptage"> Age</label>
+<input type="number" id="ptage"> <br>
+
+<!--  should save as 0 for male, 1 for female, 2 for unknown  -->
+<label for="ptgender"> Gender</label>
+<input type="number" id="ptgender"> <br>
+
+<label for="pttype"> Patient Type</label>
+<input type="text" id="pttype"> <br>
+
+<label for="ptCC"> Chief Complaint</label>
+<input type="text" id="ptCC"> <br>
+
+<!-- implement dropdown later on -->
+<label for="hospitaldestination"> Hospital Destination </label>
+<input type="text" id="hospitaldestination"> <br>
+
+<button id="notifyButton"> Notify </button>
+</form>
---
+++ front/src/routes/ems/+page.ts
@@ -0,0 +1,5 @@
+import type { PageLoad } from './ems/$types';
+
+export const load = (async () => {
+    return {};
+}) satisfies PageLoad;
